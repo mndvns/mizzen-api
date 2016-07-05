b2cc9a60de544356e6da81f49ba874873240e9e8
Doc <michael.vanasse@optiv.com>
2016-07-05 12:00:49 -0500

    add config for response store

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,6 @@ use Mix.Config
 
 config :api,
   safe_browsing_key: System.get_env("SAFE_BROWSING_KEY"),
-  virus_total_key: System.get_env("VIRUS_TOTAL_KEY")
+  virus_total_key: System.get_env("VIRUS_TOTAL_KEY"),
+
+  store_response: false
